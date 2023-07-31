2681ead611fc52929b20abc402112aae53b97dc6
defsub <defsub@defsub.com>
2023-07-31 12:47:58 -0700

    playout

---
+++ client/playout/playout.go
@@ -0,0 +1,121 @@
+// Copyright 2023 defsub
+//
+// This file is part of Takeout.
+//
+// Takeout is free software: you can redistribute it and/or modify it under the
+// terms of the GNU Affero General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option)
+// any later version.
+//
+// Takeout is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for
+// more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with Takeout.  If not, see <https://www.gnu.org/licenses/>.
+
+package playout
+
+import (
+	"github.com/spf13/viper"
+
+	"github.com/takeoutfm/takeout"
+)
+
+const (
+	UserAgent = "Playout/" + takeout.Version + " (" + takeout.Contact + ")"
+
+	TokenAccess  = "accesstoken"
+	TokenRefresh = "refreshtoken"
+	TokenMedia   = "mediatoken"
+	TokenCode    = "codetoken"
+
+	Code     = "code"
+	Endpoint = "endpoint"
+)
+
+// func systemConfig() *viper.Viper {
+// 	cfg := viper.New()
+// 	cfg.SetConfigName("config")
+// 	cfg.SetConfigType("yaml")
+// 	cfg.AddConfigPath(".")
+// 	cfg.AddConfigPath("$HOME/.config/playout")
+// 	cfg.AddConfigPath("$HOME/.playout")
+// 	cfg.AddConfigPath("/etc/playout")
+// 	return cfg
+// }
+
+// func tokensConfig() *viper.Viper {
+// 	cfg := viper.New()
+// 	cfg.SetConfigName("tokens")
+// 	cfg.SetConfigType("yaml")
+// 	cfg.AddConfigPath("$HOME/.config/playout")
+// 	return cfg
+// }
+
+type Playout struct {
+	config *viper.Viper
+	tokens *viper.Viper
+}
+
+func NewPlayout(config *viper.Viper, tokens *viper.Viper) *Playout {
+	return &Playout{config: config, tokens: tokens}
+}
+
+func (p Playout) UpdateAccessCode(code, access string) error {
+	p.tokens.Set(Code, code)
+	p.tokens.Set(TokenCode, access)
+	return p.writeTokens()
+}
+
+func (p Playout) UpdateTokens(access, refresh, media string) error {
+	p.tokens.Set(TokenAccess, access)
+	p.tokens.Set(TokenRefresh, refresh)
+	p.tokens.Set(TokenMedia, media)
+	return p.writeTokens()
+}
+
+func (p Playout) UpdateAccessToken(value string) {
+	p.tokens.Set(TokenAccess, value)
+	err := p.writeTokens()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (p Playout) writeTokens() error {
+	err := p.tokens.WriteConfig()
+	if err != nil {
+		err = p.tokens.SafeWriteConfig()
+	}
+	return err
+}
+
+func (p Playout) Endpoint() string {
+	return p.config.GetString(Endpoint)
+}
+
+func (p Playout) UserAgent() string {
+	return UserAgent
+}
+
+func (p Playout) Code() string {
+	return p.tokens.GetString(Code)
+}
+
+func (p Playout) CodeToken() string {
+	return p.tokens.GetString(TokenCode)
+}
+
+func (p Playout) AccessToken() string {
+	return p.tokens.GetString(TokenAccess)
+}
+
+func (p Playout) RefreshToken() string {
+	return p.tokens.GetString(TokenRefresh)
+}
+
+func (p Playout) MediaToken() string {
+	return p.tokens.GetString(TokenMedia)
+}
---
+++ client/playout/view.go
@@ -0,0 +1,51 @@
+// Copyright 2023 defsub
+//
+// This file is part of Takeout.
+//
+// Takeout is free software: you can redistribute it and/or modify it under the
+// terms of the GNU Affero General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option)
+// any later version.
+//
+// Takeout is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for
+// more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with Takeout.  If not, see <https://www.gnu.org/licenses/>.
+
+package playout
+
+import (
+	"fmt"
+	"github.com/takeoutfm/takeout/client/player"
+)
+
+type View interface {
+	OnStart(*player.Player)
+	OnTrack(*player.Player)
+	OnError(*player.Player, error)
+	OnStop()
+}
+
+type SimpleView struct {
+}
+
+func NewSimpleView() View {
+	return &SimpleView{}
+}
+
+func (SimpleView) OnStart(p *player.Player) {
+}
+
+func (SimpleView) OnTrack(p *player.Player) {
+	fmt.Printf("%s / %s / %s\n", p.Artist(), p.Album(), p.Title())
+}
+
+func (SimpleView) OnError(p *player.Player, err error) {
+	fmt.Printf("Error %v\n", err)
+}
+
+func (SimpleView) OnStop() {
+}
