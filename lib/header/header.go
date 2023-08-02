0fb49d4e7660f83121306ad6459e1286710aa5ad
defsub <defsub@defsub.com>
2023-08-02 10:48:07 -0700

    added

---
+++ lib/header/header.go
@@ -0,0 +1,40 @@
+// Copyright 2023 defsub
+//
+// This file is part of Takeout.
+//
+// Takeout is free software: you can redistribute it and/or modify it under the
+// terms of the GNU Affero General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option)
+// any later version.
+//
+// Takeout is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for
+// more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with Takeout.  If not, see <https://www.gnu.org/licenses/>.
+
+package header
+
+import (
+	"net/http"
+)
+
+var (
+	Authorization  = http.CanonicalHeaderKey("Authorization")
+	CacheControl   = http.CanonicalHeaderKey("Cache-Control")
+	ContentLength  = http.CanonicalHeaderKey("Content-Length")
+	ContentType    = http.CanonicalHeaderKey("Content-type")
+	ETag           = http.CanonicalHeaderKey("ETag")
+	IcyBr          = http.CanonicalHeaderKey("Icy-Br")
+	IcyDescription = http.CanonicalHeaderKey("Icy-Description")
+	IcyGenre       = http.CanonicalHeaderKey("Icy-Genre")
+	IcyMetaData    = http.CanonicalHeaderKey("Icy-MetaData")
+	IcyMetaInt     = http.CanonicalHeaderKey("Icy-MetaInt")
+	IcyName        = http.CanonicalHeaderKey("Icy-Name")
+	IcyPublic      = http.CanonicalHeaderKey("Icy-Pub")
+	IcyUrl         = http.CanonicalHeaderKey("Icy-Url")
+	LastModified   = http.CanonicalHeaderKey("Last-Modified")
+	UserAgent      = http.CanonicalHeaderKey("User-agent")
+)
