01e01cf081c6e04af32e25b925a7213fcc5ed238
defsub <defsub@defsub.com>
2024-02-02 13:19:54 -0800

    add rss tests

--- lib/rss/rss_test.go
+++ lib/rss/rss_test.go
@@ -1,34 +1,100 @@
 package rss
 
 import (
-	"github.com/takeoutfm/takeout/lib/client"
-	//"github.com/takeoutfm/takeout/lib/date"
+	"bytes"
+	"embed"
+	"io/ioutil"
+	"net/http"
+	"strings"
 	"testing"
+
+	"github.com/takeoutfm/takeout/lib/client"
 )
 
-func TestRSS(t *testing.T) {
-	urls := []string{
-		// "https://feeds.twit.tv/twit.xml",
-		// "https://feeds.twit.tv/sn.xml",
-		// "https://www.pbs.org/newshour/feeds/rss/podcasts/show",
-		"http://feeds.feedburner.com/TEDTalks_audio",
-	}
+//go:embed test/*.xml
+var jsonFiles embed.FS
 
-	var config client.Config
-	config.UserAgent = "rss/test"
-	rss := NewRSS(client.NewGetter(config))
-	for i := 0; i < len(urls); i++ {
-		url := urls[i]
-		feed, err := rss.Fetch(url)
-		if err != nil {
-			t.Logf("%v\n", err)
+func jsonFile(name string) string {
+	d, err := jsonFiles.ReadFile(name)
+	if err != nil {
+		return ""
+	}
+	return string(d)
+}
+
+type rssServer struct {
+	t *testing.T
+}
+
+func (s rssServer) RoundTrip(r *http.Request) (*http.Response, error) {
+	var body string
+	//s.t.Logf("got %s\n", r.URL.String())
+	if strings.HasPrefix(r.URL.Path, "/twit.xml") {
+		body = jsonFile("test/twit.xml")
+	}
+	return &http.Response{
+		StatusCode: 200,
+		Body:       ioutil.NopCloser(bytes.NewBufferString(body)),
+		Header:     make(http.Header),
+	}, nil
+}
+
+func makeClient(t *testing.T) *RSS {
+	c := client.NewTransportGetter(client.Config{UserAgent: "test/1.0"}, rssServer{t: t})
+	return NewRSS(c)
+}
+
+func TestFetch(t *testing.T) {
+	r := makeClient(t)
+	c, err := r.Fetch("https://feeds.twit.tv/twit.xml")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if c == nil {
+		t.Fatal("expect channel")
+	}
+	if c.Title != "This Week in Tech (Audio)" {
+		t.Error("expect title")
+	}
+	if c.Author != "TWiT" {
+		t.Error("expect author")
+	}
+	if c.Link() != "https://twit.tv/shows/this-week-in-tech" {
+		t.Error("expect link")
+	}
+	if len(c.Image.Link) == 0 {
+		t.Error("expect image link")
+	}
+	if len(c.Items) == 0 {
+		t.Error("expect items")
+	}
+	for _, i := range c.Items {
+		if len(i.Title) == 0 {
+			t.Error("expect item title")
 		}
-		t.Logf("%s [%s]\n", feed.Title, feed.Link())
-		//t.Logf("%s\n", channel.LastBuildTime())
-		for _, e := range feed.Items {
-			//t.Logf("%s - %s\n", date.Format(e.PublishTime), e.Title)
-			//t.Logf("%s %d %s\n", e.ContentType, e.Size, e.URL)
-			t.Logf("%s %s\n", e.Title, e.ItemImage())
+		if len(i.Author) == 0 {
+			t.Error("expect item author")
+		}
+		if len(i.Description) == 0 {
+			t.Error("expect item description")
+		}
+		if strings.HasPrefix(i.URL(), "http") == false {
+			t.Error("expect item link")
+		}
+		if len(i.GUID.Value) == 0 {
+			t.Error("expect guid value")
+		}
+		if strings.Contains(i.ItemGUID(), "://") {
+			t.Error("expect guid is not url")
+		}
+		if len(i.PubDate) == 0 {
+			t.Error("expect pubdate")
+		}
+		if i.PublishTime().IsZero() {
+			t.Error("expect valid pubdate")
+		}
+		if len(i.ItemImage()) == 0 {
+			t.Error("expect item image")
 		}
 	}
 }
